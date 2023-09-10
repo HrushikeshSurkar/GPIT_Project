27d2c6bedf99f91d6e4cb97d98551b43d97f1a51
Hrushikesh <hrushikesh.surkar@greatplaceitservices.com>
2023-09-10 21:12:48 +0530

    done with task

--- src/app/modules/main/text/text.component.ts
+++ src/app/modules/main/text/text.component.ts
@@ -8,12 +8,41 @@ import { TextService } from 'src/app/shared/api/text.service';
   styleUrls: ['./text.component.scss']
 })
 export class TextComponent {
+  tagData: any;
+  toppings = new FormControl([]);
+  filteredTagData: any;
+  selectedValues: string[] = []; // Store selected values here
 
-  tagData: any
-  tagName: any
-  tagOption: any
-  toppings = new FormControl('');
   constructor(private data: TextService) {
-    this.tagData = this.data.getData()
+    this.tagData = this.data.getData();
+    this.filteredTagData = this.tagData; // Initialize with all data
+  }
+
+  // Function to handle selection change in mat-select
+  onSelectionChange(event: any) {
+    this.selectedValues = event.value;
+  }
+
+  // Function to check if a value is selected
+  isSelected(value: string): boolean {
+    return this.selectedValues.includes(value);
+  }
+
+  filterOptions(searchTerm: string) {
+    if (!searchTerm) {
+      // If the search term is empty, show all options
+      this.filteredTagData = this.tagData;
+      return;
+    }
+
+    // Filter options based on the search term and selected values
+    this.filteredTagData = this.tagData.map((group: any[][]) => [
+      group[0],
+      group[1].filter(item => {
+        const isMatch = item.key.toLowerCase().includes(searchTerm.toLowerCase());
+        const isSelected = this.selectedValues.includes(item.key);
+        return isMatch || isSelected;
+      })
+    ]);
   }
 }
