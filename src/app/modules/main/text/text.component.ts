f4b327f62a1570cf7e54ce6f0bc512180b1fe3ae
Hrushikesh <hrushikesh.surkar@greatplaceitservices.com>
2023-09-10 20:39:52 +0530

    working

--- src/app/modules/main/text/text.component.ts
+++ src/app/modules/main/text/text.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { TextService } from 'src/app/shared/api/text.service';
 
 @Component({
@@ -8,4 +9,11 @@ import { TextService } from 'src/app/shared/api/text.service';
 })
 export class TextComponent {
 
+  tagData: any
+  tagName: any
+  tagOption: any
+  toppings = new FormControl('');
+  constructor(private data: TextService) {
+    this.tagData = this.data.getData()
+  }
 }
--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -64,7 +64,7 @@ export const MaterialComponents = [
   MatSnackBarModule,
   MatTableModule,
   MatSortModule,
-  MatPaginatorModule
+  MatPaginatorModule,
 ]
 
 
